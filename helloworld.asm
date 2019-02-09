50709570f543097a3a60bcd626ab8d3e2a9768a3
P4B5 <37982892+P4B5@users.noreply.github.com>
2019-02-09 14:44:31 +0100

    Update helloworld.asm

--- helloworld.asm
+++ helloworld.asm
@@ -1,12 +1,12 @@
 
 .data 
 
-str: .asciz "Hello world in RISC-V!\n"
+str: .asciz "Hello world in RISC-V!\n"  #string with asccii characters
 
 .text
 
-main:  la a0, str #load adress (in a0 storage the string hello world
-	li a7, 4  #load imm
+main:   la a0, str #load adress (in a0 storage the string hello world)
+	li a7, 4  #load imm, number 4 in adress a7
 	ecall    #trasnfer the control to the system (0S)
 	li a7, 10
 	ecall
