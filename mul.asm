61bb8230304415bbf08f432901841ac58b7d2e3d
P4B5 <37982892+P4B5@users.noreply.github.com>
2019-02-16 20:57:57 +0100

    Add files via upload

---
+++ mul.asm
@@ -0,0 +1,33 @@
+
+#THIS ASSEMBLY PROGRAM SUM TWO INTEGER NUMBERS
+
+.data
+
+str: .asciz "the result is: "
+
+.text 
+
+main:
+
+	
+	
+
+	li a7, 5    #INTRODUCE THE FIRST NUMBER
+	ecall
+	mv t0, a0
+	
+	li a7, 5
+	ecall
+	mv t1, a0
+	
+	mul t3, t0, t1
+	
+	la a0, str 
+	li a7, 4
+	ecall
+	
+	mv a0, t3
+	li a7, 1
+	ecall
+	
+	
